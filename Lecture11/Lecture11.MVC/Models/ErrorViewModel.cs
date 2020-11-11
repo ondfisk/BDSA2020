21861a5620b8fc93882f68031820062c2580c0c7
Rasmus Lystrøm <rasmus@lystroem.dk>
2020-11-11 09:51:39 +0100

    Lecture11 (#11)
    * 11
    
    * done
    
    Co-authored-by: Rasmus Lystrøm <rasmusl@microsoft.com>

---
+++ Lecture11/Lecture11.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lecture11.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
